6cc8324aa85f430267ff945a057ee4352d1604f4
James Lee <jameslee1187@gmail.com>
2019-09-20 15:20:42 -0400

    added app listener

--- src/server.js
+++ src/server.js
@@ -1,1 +1,10 @@
 import express from "express"
+
+const PORT = 4000;
+const app = express();
+
+const handleListening = () => {
+    console.log("server started")
+};
+
+app.listen(PORT, handleListening);
