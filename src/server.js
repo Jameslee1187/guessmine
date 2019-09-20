6f0a2da44f3cd67eb542de82e03e8fbeba312309
James Lee <jameslee1187@gmail.com>
2019-09-20 15:25:13 -0400

    configured pug with server

--- src/server.js
+++ src/server.js
@@ -1,10 +1,15 @@
+import {
+    join
+} from "path"
 import express from "express"
 
 const PORT = 4000;
 const app = express();
-app.set("views engine", "pug")
-const handleListening = () => {
-    console.log("server started")
-};
+app.set("view engine", "pug")
+app.set("views", join(__dirname, "views") const handleListening = () => {
+            console.log("server started")
+        };
 
-app.listen(PORT, handleListening);
+        app.get("/", (req, res) => res.render("home"))
+
+        app.listen(PORT, handleListening);
